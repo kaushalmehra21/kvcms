acbf6b9a3e3d4fbbf5391f7e7bea2af1d31221c1
KAUSHAL-PC\Kaushal <kaushalmehra@live.com>
2017-05-25 18:35:30 +0530

    update DB change extention integer to varchar

--- documents/cake_test.sql
+++ documents/cake_test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2017 at 11:00 AM
+-- Generation Time: May 25, 2017 at 03:03 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -35,14 +35,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `parent_id` int(11) DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `title`, `short_description`, `description`, `image`, `parent_id`, `created`, `modified`) VALUES
-(3, 'uncategorised', 'uncategorised', 'uncategorised', 'uncategorised', NULL, '2017-05-18 12:28:01', '2017-05-18 12:36:58');
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -76,14 +69,7 @@ CREATE TABLE IF NOT EXISTS `companies` (
   `user_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `companies`
---
-
-INSERT INTO `companies` (`id`, `title`, `description`, `logo`, `banner`, `user_id`, `created`, `modified`) VALUES
-(1, 'Saglus Info pvt. ltd.', 'Saglus Info pvt. ltd.', 'Saglus Info pvt. ltd', 'Saglus Info pvt. ltd', 3, '2017-05-13 09:46:09', '2017-05-13 09:53:53');
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -112,13 +98,13 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `name` varchar(200) NOT NULL,
   `short_description` varchar(200) DEFAULT NULL,
   `description` text,
-  `extention` int(11) NOT NULL,
+  `extention` varchar(20) NOT NULL,
   `type` varchar(50) NOT NULL,
   `size` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -135,15 +121,7 @@ CREATE TABLE IF NOT EXISTS `menus` (
   `parent_id` int(11) DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `menus`
---
-
-INSERT INTO `menus` (`id`, `title`, `short_description`, `url`, `serial_number`, `parent_id`, `created`, `modified`) VALUES
-(1, 'Home', 'home', 'home', 1, NULL, '2017-05-18 12:50:51', '2017-05-18 12:50:51'),
-(2, 'About Us', 'About Us', 'about-us', 2, NULL, '2017-05-18 12:51:15', '2017-05-18 12:51:15');
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -166,13 +144,6 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `title`, `slug`, `short_description`, `description`, `category_id`, `user_id`, `post_type_id`, `status`, `featured_image`, `created`, `modified`) VALUES
-(1, 'Toolbar Location', 'toolbar_location', 'Toolbar Location', '<p>CKEditor allows you to customize the editor toolbar location. By default, in&nbsp;<a href="http://docs.ckeditor.com/#!/guide/dev_framed">classic</a>,&nbsp;iframe-based editors the toolbar is placed in the top part of the editor UI, above the editing area.</p>\r\n\r\n<p>You can, however, use the&nbsp;<a href="http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-toolbarLocation" rel="CKEDITOR.config-cfg-toolbarLocation">CKEDITOR.config.toolbarLocation</a>&nbsp;configuration setting to change this option. For the default editor implementation you can choose between the&nbsp;top&nbsp;(the default) and the&nbsp;bottom&nbsp;setting.</p>\r\n\r\n<p>For example, to put the toolbar at the bottom of the interface, use the following option:</p>\r\n\r\n<p>config.toolbarLocation = &#39;bottom&#39;;</p>\r\n\r\n<p>To receive the result such as this:</p>\r\n', 3, 2, 1, 1, 'toolbar_location', '2017-05-18 12:38:21', '2017-05-18 12:38:21');
-
 -- --------------------------------------------------------
 
 --
@@ -184,7 +155,7 @@ CREATE TABLE IF NOT EXISTS `posts_documents` (
   `post_id` int(11) NOT NULL,
   `document_id` int(11) NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -197,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `posts_tags` (
   `post_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL,
   `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -228,13 +199,6 @@ CREATE TABLE IF NOT EXISTS `post_types` (
   `created` datetime NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `post_types`
---
-
-INSERT INTO `post_types` (`id`, `title`, `description`, `modified`, `created`) VALUES
-(1, 'post', 'post', '2017-05-18 12:37:29', '2017-05-18 12:37:29');
-
 -- --------------------------------------------------------
 
 --
@@ -247,14 +211,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `description` varchar(200) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`id`, `title`, `description`, `created`, `modified`) VALUES
-(1, 'admin', 'admin', '2017-05-13 09:42:48', '2017-05-13 09:42:48');
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -266,7 +223,7 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -283,15 +240,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `activation_key` varchar(100) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `role_id`, `activation_key`, `created`, `modified`) VALUES
-(2, 'user1', '$2y$10$NGGWF68lrByjk4ao4vUDDehnjWKq4DDuIeyRS3jT2QvC.oiyQQYXW', 'user1@eduncle.com', 1, '12345', '2017-05-13 09:51:53', '2017-05-13 09:51:53'),
-(3, 'kaushal', '$2y$10$AgTssglewo8TAbwOq18Cbug8kFa5HprmooGN6nNH0YpdOCuUGPB9S', 'kaushal@eduncle.com', 1, '12345', '2017-05-13 09:53:42', '2017-05-13 12:11:08');
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -427,7 +376,7 @@ ALTER TABLE `user_meta`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `comments`
 --
@@ -437,7 +386,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `company_meta`
 --
@@ -447,12 +396,12 @@ ALTER TABLE `company_meta`
 -- AUTO_INCREMENT for table `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `posts`
 --
@@ -462,12 +411,12 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `posts_documents`
 --
 ALTER TABLE `posts_documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `posts_tags`
 --
 ALTER TABLE `posts_tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `post_meta`
 --
@@ -482,17 +431,17 @@ ALTER TABLE `post_types`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user_meta`
 --
@@ -522,13 +471,6 @@ ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`post_type_id`) REFERENCES `post_types` (`id`);
-
---
--- Constraints for table `posts_tags`
---
-ALTER TABLE `posts_tags`
-  ADD CONSTRAINT `posts_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
-  ADD CONSTRAINT `posts_tags_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
 
 --
 -- Constraints for table `users`
